7aa000fe88cd90321e7530c9e1f6e1aae9666c95
chaewoon-Front <treechae38@naver.com>
2022-12-09 02:24:37 +0900

    todo 시험 초안

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [title, setTitle] = useState("");
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: "오채운 리액트 배우자",
+    },
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="text"
+        value={title}
+        onChange={(event) => {
+          setTitle(event.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          setTodos([...todos, { id: todos.length + 1, title: title }]);
+        }}
+      >
+        추가하기
+      </button>
+      <h1>Todo List</h1>
+      <div className="todos-container">{}</div>
     </div>
   );
 }
